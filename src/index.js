f04b799bc372f538e802ee2d82b384d4238a048a
Shane Brady <sbrady316@hotmail.com>
2022-02-11 00:49:30 -0800

    add API

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-async function getUserInfo() {
+async function getClientUserInfo() {
     const response = await fetch('/.auth/me');
     const rt = await response.text();
     console.log(`User Info: ${rt}`);
 
     const { clientPrincipal } = JSON.parse(rt);
     return clientPrincipal;
-  }
+}
 
- async function main(elementName) {
-    var user = await getUserInfo();
+async function getServerUserInfo() {
+    const { text } = await( await fetch(`/api/message`)).text();
+
+    return text;
+}
+
+async function updateElement(elementName, infoProvider) {
+    var user = await infoProvider();
     console.log(`Going to update element ${elementName}`);
     var element = document.getElementById(elementName);
     console.log(element);
     element.innerText = user.userDetails;
-  }
+}
 
-  main("clientUserInfo");
+updateElement("clientUserInfo", getClientUserInfo);
+updateElement("serverUserInfo", getServerUserInfo);
