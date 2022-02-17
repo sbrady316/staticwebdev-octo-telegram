39129205c7f063d28338625fd8fe4ac481dc8c9a
Shane Brady <sbrady316@hotmail.com>
2022-02-16 21:50:34 -0800

    Check for authentication

--- src/api/AuthInfo/index.js
+++ src/api/AuthInfo/index.js
@@ -1,13 +1,33 @@
 module.exports = async function (context, req) {
+    var principal;
+
     const header = req.headers['x-ms-client-principal'];
-    const encoded = Buffer.from(header, 'base64');
-    const decoded = encoded.toString('ascii');
+    if (header != null)
+    {
+        const encoded = Buffer.from(header, 'base64');
+        const decoded = encoded.toString('ascii');
+        principal: JSON.parse(decoded),
+    }
+    else
+    {
+        principal = {
+            "clientPrincipal": {
+              "identityProvider": "aad",
+              "userId": "0db7780d7985465098c512c58eeac286",
+              "userDetails": "sbrady@somewhere.com",
+              "userRoles": [
+                "anonymous",
+                "authenticated"
+              ]
+            }
+          };
+    }
 
     context.log(`Returning auth info as ${decoded}`);
 
     context.res = {
       body: {
-        clientPrincipal: JSON.parse(decoded),
+          principal
       },
     };
   };
